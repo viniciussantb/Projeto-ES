9fdc8cfb9e0ede1e82cf2342eca87f7b13cbd102
viniciussantb <55957552+viniciussantb@users.noreply.github.com>
2021-10-21 21:52:07 -0300

    login crontroller
    Setup of login controllers
    Finding user on database

--- application/controllers/login.js
+++ application/controllers/login.js
@@ -1,3 +1,15 @@
 module.exports.getLogin= function(app, req, res){
     res.render('login');
 }
+
+module.exports.userLogin = async function(app, req, res){
+    const userModel = require('../models/user');
+    var bodyData = req.body;
+    var users = await userModel.findOne(bodyData);
+
+    try{
+        res.send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
--- application/routes/login.js
+++ application/routes/login.js
@@ -2,4 +2,8 @@ module.exports = function(app){
     app.get('/login', function(req, res){
         app.application.controllers.login.getLogin(app, req, res);
     });
+
+    app.post('/user_login', function(req, res){
+        app.application.controllers.login.userLogin(app, req, res);
+    })
 };
