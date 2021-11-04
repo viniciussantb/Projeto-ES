bf7714dd78ae3bee9a095c6be4a9f3ebd4b9500f
viniciussantb <55957552+viniciussantb@users.noreply.github.com>
2021-11-04 15:35:47 -0300

    todoList  - toggleReminder
    setting the toggleReminder function

--- Entregas/Hackenges/TodoList/src/App.js
+++ Entregas/Hackenges/TodoList/src/App.js
@@ -13,20 +13,30 @@ function App() {
     },
     {
       id : 2,
+      text : 'Gym',
+      date : '5th feb 2010 4:30pm',
+      reminder : true
+    },
+    {
+      id : 3,
       text : 'School Appointment',
       date : '5th feb 2010 6:40pm',
       reminder : true
     }
   ]);
 
-  const onDelete = (id)=>{
+  const onDelete = (id) => {
     setTask(tasks.filter((task)=>task.id !== id));
+  }
+
+  const toggleReminder = (id) => {
+    setTask(tasks.map((task) => task.id === id ? {...task, reminder : !task.reminder} : task));
   }
 
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks} onDelete={onDelete}/>
+      <Tasks tasks={tasks} onDelete={onDelete} toggleReminder={toggleReminder}/>
     </div>
   );
 }
--- Entregas/Hackenges/TodoList/src/components/Task.js
+++ Entregas/Hackenges/TodoList/src/components/Task.js
@@ -1,9 +1,9 @@
 import {FaTimes} from 'react-icons/fa'
 
-function Task({ task, onDelete }) {
+function Task({ task, onDelete, toggleReminder}) {
 
     return (
-        <div className='task'>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={()=>toggleReminder(task.id)}>
             <h3>{task.text} <FaTimes onClick={()=> onDelete(task.id)}/></h3>
             <p>{task.date}</p>
         </div>
