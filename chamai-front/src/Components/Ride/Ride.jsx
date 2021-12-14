5b7f0eaa07263d5b88168379c10fa0c49e205a45
vnmoliveira <76496863+vnmoliveira@users.noreply.github.com>
2021-12-14 03:36:39 -0300

    Previa finalização da tela do feed para testes de integração com o back-end

--- chamai-front/src/Components/Ride/Ride.jsx
+++ chamai-front/src/Components/Ride/Ride.jsx
@@ -4,7 +4,34 @@ import styles from './Ride.module.css'
 function Ride() {
     return (
         <div className={styles.rideContainer}>
-            Carona
+            <div className={styles.userPerfil}>
+                <div className={styles.userPhoto}>
+
+                </div>
+
+                <div className={styles.userData}>
+                    <p>Nome</p>
+                    <p>Faculdade</p>
+            
+                </div>
+            
+            </div>
+
+            <div className={styles.rideData}>
+                <div className={styles.rideDataLeft}>
+                    <p>Partida</p>
+                    <p>Chegada</p>
+                    <p>Valor</p>
+
+                </div>
+
+                <div className={styles.rideDataRight}>
+                    <p>Data</p>
+                    <p>Hora</p>
+                    <p>Vagas</p>
+
+                </div>
+            </div>
         </div>
     )
 }
